24de1030de96adb169e2626da55fd168a548df20
Limenal <lshrims@gmail.com>
2021-10-29 20:36:27 +0500

    Add hour and minute intervals for rebases

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 // const axios = require('axios')
 import {getStakesInfoDays, getStakesInfoHour, getStakesInfoMinute, getStakesInfoNHour} from './stakes.js'
 import {getDepositsInfoDays, getDepositsInfoHours, getDepositsInfoMinutes, getDepositsInfoNHours} from './deposits.js'
-import {getRebasesInfoDays} from './rebases.js'
+import {getRebasesInfoDays, getRebasesInfoHours, getRebasesInfoMinutes, getRebasesInfoNHours} from './rebases.js'
 
 async function main()
 {
     // const stakeData = await getStakesInfo(1632268840, 3600, 24)
     // const depositData = 
-    const deposit = await getDepositsInfoDays(1634342400, 1634774400)
+    const deposit = await getRebasesInfoNHours(1616716800, 1616803200, 8)
     // if(!!depositMinute)
     // {
     for(let i = 0; i < deposit.length; ++i)
     {
 
-            console.log(deposit[i])
+        console.log(deposit[i])
         
     }
     console.log(deposit.length)
 
-    // }
+    
 }
 
 main()
--- rebases.js
+++ rebases.js
@@ -24,7 +24,6 @@ export async function getRebasesInfoDays(startTimestamp, endTime)
       }
       
     `
-      console.log(rebaseQuery)
     try
     {
       const rebaseData = await axios({
@@ -36,13 +35,25 @@ export async function getRebasesInfoDays(startTimestamp, endTime)
       }) 
       const rebasesData = rebaseData.data.data.rebaseYears
       let data = []
+      let lastApy = 0
       for(let k = 0; k < rebasesData.length; ++k)
       {
         for(let i = 0; i < rebasesData[k].dayRebase.length; ++i)
         {
           let obj = {}
           obj.percentage = rebasesData[k].dayRebase[i].percentage
-          obj.apy = Math.pow((1 + Number(rebasesData[k].dayRebase[i].percentage)), 1095)
+          let apy = Math.pow((1 + Number(rebasesData[k].dayRebase[i].percentage)), 1095)
+          if(apy > 100000)
+          {
+            if(lastApy != 0)
+            {
+              obj.apy = lastApy
+            }
+          }
+          else
+          {
+            obj.apy = apy
+          }
           obj.timestamp = rebasesData[k].dayRebase[i].timestamp
           data.push(obj)
         }
@@ -52,5 +63,265 @@ export async function getRebasesInfoDays(startTimestamp, endTime)
     catch(err)
     {
         console.log(err)
+    }
+}
+
+export async function getRebasesInfoHours(startTimestamp, endTime)
+{
+    
+    let rebaseQuery = `
+
+    {
+        rebaseYears(first: 3){
+          dayRebase(first:365 orderBy:timestamp where:{timestamp_gte: ${startTimestamp}, timestamp_lt:${endTime} }){
+            hourRebase(first: 24 orderBy:timestamp)
+            {
+              
+                percentage
+                id
+                timestamp
+              
+            }
+          }
+        }
+      }
+      
+    `
+    try
+    {
+      const rebaseData = await axios({
+          url: 'https://api.thegraph.com/subgraphs/id/QmVknJSJUdQEALEBciLHGoaTBQB6ndAxMViKcsjQAjSmne',
+          method: 'post',
+          data: {
+            query: rebaseQuery
+          }
+      }) 
+      const rebasesData = rebaseData.data.data.rebaseYears
+      let data = []
+      let resData = []
+      for(let k = 0; k < rebasesData.length; ++k)
+      {
+        for(let i = 0; i < rebasesData[k].dayRebase.length; ++i)
+        {
+          for(let j = 0; j < rebasesData[k].dayRebase[i].hourRebase.length; ++j)
+          {
+            let obj = {}
+            obj.percentage = rebasesData[k].dayRebase[i].hourRebase[j].percentage
+            let apy = Math.pow((1 + Number(rebasesData[k].dayRebase[i].hourRebase[j].percentage)), 1095)
+            
+            obj.apy = apy
+              
+            obj.timestamp = rebasesData[k].dayRebase[i].hourRebase[j].timestamp
+            data.push(obj)  
+          }
+        }
+      }
+      let prevPercentage = 0
+      let prevApy = 0
+      for(let beginTimestamp = startTimestamp, endTimestamp = startTimestamp + 3600; beginTimestamp < endTime; beginTimestamp += 3600, endTimestamp+= 3600)
+      {
+        
+        let obj = {
+          beginTimestamp: beginTimestamp,
+          endTimestamp: endTimestamp,
+          percentage: prevPercentage,
+          apy: prevApy,
+        }
+        for(let j = 0; j < data.length; ++j)
+        {
+          if(beginTimestamp <= data[j].timestamp && data[j].timestamp < endTimestamp)
+          {
+            if(Number(data[j].apy) < 1000)
+            {
+              obj.apy = data[j].apy
+              obj.percentage = data[j].percentage
+              prevPercentage = data[j].percentage
+              prevApy = data[j].apy
+            }            
+          }
+        }
+        resData.push(obj)
+      }      
+      return resData
+    }
+    catch(err)
+    {
+      console.log(err)
+    }
+}
+
+export async function getRebasesInfoNHours(startTimestamp, endTime, hours)
+{
+    
+    let rebaseQuery = `
+
+    {
+        rebaseYears(first: 3){
+          dayRebase(first:365 orderBy:timestamp where:{timestamp_gte: ${startTimestamp}, timestamp_lt:${endTime} }){
+            hourRebase(first: 24 orderBy:timestamp)
+            {
+              
+                percentage
+                id
+                timestamp
+              
+            }
+          }
+        }
+      }
+      
+    `
+    try
+    {
+      const rebaseData = await axios({
+          url: 'https://api.thegraph.com/subgraphs/id/QmVknJSJUdQEALEBciLHGoaTBQB6ndAxMViKcsjQAjSmne',
+          method: 'post',
+          data: {
+            query: rebaseQuery
+          }
+      }) 
+      const rebasesData = rebaseData.data.data.rebaseYears
+      let data = []
+      let resData = []
+      for(let k = 0; k < rebasesData.length; ++k)
+      {
+        for(let i = 0; i < rebasesData[k].dayRebase.length; ++i)
+        {
+          for(let j = 0; j < rebasesData[k].dayRebase[i].hourRebase.length; ++j)
+          {
+            let obj = {}
+            obj.percentage = rebasesData[k].dayRebase[i].hourRebase[j].percentage
+              
+            obj.timestamp = rebasesData[k].dayRebase[i].hourRebase[j].timestamp
+            data.push(obj)  
+          }
+        }
+      }
+      let prevPercentage = 0
+      let prevApy = 0
+      for(let beginTimestamp = startTimestamp, endTimestamp = startTimestamp + hours*3600; beginTimestamp < endTime; beginTimestamp += hours*3600, endTimestamp+= hours*3600)
+      {
+        let rebasesCount = 0 
+        let percentageSum = 0
+
+        let obj = {
+          beginTimestamp: beginTimestamp,
+          endTimestamp: endTimestamp,
+          percentage: prevPercentage,
+          apy: prevApy,
+        }
+        for(let j = 0; j < data.length; ++j)
+        {
+          if(beginTimestamp <= data[j].timestamp && data[j].timestamp < endTimestamp)
+          {
+            rebasesCount++
+            percentageSum += data[j].percentage
+          }
+        }
+        let apy = Math.pow((1 + (Number(percentageSum)/rebasesCount)), 1095)
+        if(apy < 1000)
+        {
+          obj.apy = apy
+          obj.percentage = Number(percentageSum)/rebasesCount
+          prevPercentage = Number(percentageSum)/rebasesCount
+          prevApy = apy
+        }
+
+        resData.push(obj)
+      }      
+      return resData
+    }
+    catch(err)
+    {
+      console.log(err)
+    }
+}
+
+export async function getRebasesInfoMinutes(startTimestamp, endTime)
+{
+    
+    let rebaseQuery = `
+
+    {
+        rebaseYears(first: 3){
+          dayRebase(first:365 orderBy:timestamp where:{timestamp_gte: ${startTimestamp}, timestamp_lt:${endTime} }){
+            hourRebase(first: 24 orderBy:timestamp)
+            {
+              minuteRebase(first:60 orderBy:timestamp)
+              {
+                percentage
+                id
+                timestamp
+  
+              }
+            }
+          }
+        }
+      }
+      
+    `
+    try
+    {
+      const rebaseData = await axios({
+          url: 'https://api.thegraph.com/subgraphs/id/QmVknJSJUdQEALEBciLHGoaTBQB6ndAxMViKcsjQAjSmne',
+          method: 'post',
+          data: {
+            query: rebaseQuery
+          }
+      }) 
+      const rebasesData = rebaseData.data.data.rebaseYears
+      let data = []
+      let resData = []
+      for(let k = 0; k < rebasesData.length; ++k)
+      {
+        for(let i = 0; i < rebasesData[k].dayRebase.length; ++i)
+        {
+          for(let j = 0; j < rebasesData[k].dayRebase[i].hourRebase.length; ++j)
+          {
+            for(let c = 0; c < rebasesData[k].dayRebase[i].hourRebase[j].minuteRebase.length; ++c)
+            {
+              let obj = {}
+              obj.percentage = rebasesData[k].dayRebase[i].hourRebase[j].minuteRebase[c].percentage
+              let apy = Math.pow((1 + Number(rebasesData[k].dayRebase[i].hourRebase[j].minuteRebase[c].percentage)), 1095)
+              
+              obj.apy = apy
+                
+              obj.timestamp = rebasesData[k].dayRebase[i].hourRebase[j].minuteRebase[c].timestamp
+              data.push(obj)  
+  
+            }
+          }
+        }
+      }
+      let prevPercentage = 0
+      let prevApy = 0
+      for(let beginTimestamp = startTimestamp, endTimestamp = startTimestamp + 60; beginTimestamp < endTime; beginTimestamp += 60, endTimestamp+= 60)
+      {
+        let obj = {
+          beginTimestamp: beginTimestamp,
+          endTimestamp: endTimestamp,
+          percentage: prevPercentage,
+          apy: prevApy,
+        }
+        for(let j = 0; j < data.length; ++j)
+        {
+          if(beginTimestamp <= data[j].timestamp && data[j].timestamp < endTimestamp)
+          {
+            if(Number(data[j].apy) < 1000)
+            {
+              obj.apy = data[j].apy
+              obj.percentage = data[j].percentage
+              prevPercentage = data[j].percentage
+              prevApy = data[j].apy
+            }            
+          }
+        }
+        resData.push(obj)
+      }      
+      return resData
+    }
+    catch(err)
+    {
+      console.log(err)
     }
 }
