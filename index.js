3dfbdb5e0aca470dae54985eba9d4e1d870d2404
Limenal <lshrims@gmail.com>
2021-11-02 16:35:48 +0500

    Add nMinutes for rebases

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // const axios = require('axios')
 import {getStakesInfoDays, getStakesInfoHour, getStakesInfoMinute, getStakesInfoNHour, getStakesInfoNDays, getStakesInfoNMinute} from './stakes.js'
 import {getDepositsInfoDays, getDepositsInfoHours, getDepositsInfoMinutes, getDepositsInfoNHours, getDepositsInfoNDays, getDepositsInfoNMinutes} from './deposits.js'
-import {getRebasesInfoDays, getRebasesInfoHours, getRebasesInfoMinutes, getRebasesInfoNHours} from './rebases.js'
+import {getRebasesInfoDays, getRebasesInfoHours, getRebasesInfoMinutes, getRebasesInfoNHours, getRebasesInfoNMinutes} from './rebases.js'
 
 async function main()
 {
     // const stakeData = await getStakesInfo(1632268840, 3600, 24)
     // const depositData = 
-    const deposit = await getDepositsInfoNMinutes(1629936000, 1630108800, 120)
+    const deposit = await getRebasesInfoNMinutes(1629936000, 1630108800, 120)
     
     for(let i = 0; i < deposit.length; ++i)
     {
--- rebases.js
+++ rebases.js
@@ -424,3 +424,99 @@ export async function getRebasesInfoMinutes(startTimestamp, endTime)
       console.log(err)
     }
 }
+
+export async function getRebasesInfoNMinutes(startTimestamp, endTime, minutes)
+{
+    
+    let rebaseQuery = `
+
+    {
+        rebaseYears(first: 3){
+          dayRebase(first:365 orderBy:timestamp where:{timestamp_gte: ${startTimestamp}, timestamp_lt:${endTime} }){
+            hourRebase(first: 24 orderBy:timestamp)
+            {
+              minuteRebase(first:60 orderBy:timestamp)
+              {
+                percentage
+                id
+                timestamp
+  
+              }
+            }
+          }
+        }
+      }
+      
+    `
+    try
+    {
+      const rebaseData = await axios({
+          url: 'https://api.thegraph.com/subgraphs/id/QmVknJSJUdQEALEBciLHGoaTBQB6ndAxMViKcsjQAjSmne',
+          method: 'post',
+          data: {
+            query: rebaseQuery
+          }
+      }) 
+      const rebasesData = rebaseData.data.data.rebaseYears
+      let data = []
+      let resData = []
+      for(let k = 0; k < rebasesData.length; ++k)
+      {
+        for(let i = 0; i < rebasesData[k].dayRebase.length; ++i)
+        {
+          for(let j = 0; j < rebasesData[k].dayRebase[i].hourRebase.length; ++j)
+          {
+            for(let c = 0; c < rebasesData[k].dayRebase[i].hourRebase[j].minuteRebase.length; ++c)
+            {
+              let obj = {}
+              obj.percentage = rebasesData[k].dayRebase[i].hourRebase[j].minuteRebase[c].percentage
+              let apy = Math.pow((1 + Number(rebasesData[k].dayRebase[i].hourRebase[j].minuteRebase[c].percentage)), 1095)
+              
+              obj.apy = apy
+                
+              obj.timestamp = rebasesData[k].dayRebase[i].hourRebase[j].minuteRebase[c].timestamp
+              data.push(obj)  
+  
+            }
+          }
+        }
+      }
+      let prevPercentage = 0
+      let prevApy = 0
+      for(let beginTimestamp = startTimestamp, endTimestamp = startTimestamp + minutes*60; beginTimestamp < endTime; beginTimestamp += minutes*60, endTimestamp+= minutes*60)
+      {
+        let rebasesCount = 0 
+        let percentageSum = 0
+
+        let obj = {
+          beginTimestamp: beginTimestamp,
+          endTimestamp: endTimestamp,
+          percentage: prevPercentage,
+          apy: prevApy,
+        }
+        for(let j = 0; j < data.length; ++j)
+        {
+          if(beginTimestamp <= data[j].timestamp && data[j].timestamp < endTimestamp)
+          {
+            rebasesCount++
+            percentageSum += data[j].percentage
+          }
+        }
+        let apy = Math.pow((1 + (Number(percentageSum)/rebasesCount)), 1095)
+        if(apy < 1000)
+        {
+          obj.apy = apy
+          obj.percentage = Number(percentageSum)/rebasesCount
+          prevPercentage = Number(percentageSum)/rebasesCount
+          prevApy = apy
+        }
+
+        resData.push(obj)
+      }      
+      return resData
+    }
+    catch(err)
+    {
+      console.log(err)
+    }
+}
